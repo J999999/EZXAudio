343ff793b6803067eb6bca879cb82df96f78a237
zhangyu <zhang.yu@ngsense.com>
2022-08-31 19:31:59 +0800

    fitst commit

---
+++ EZXAudio/EZXAudio.h
@@ -0,0 +1,18 @@
+//
+//  EZXAudio.h
+//  EZXAudio
+//
+//  Created by MacBook Pro on 2022/8/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EZXAudio.
+FOUNDATION_EXPORT double EZXAudioVersionNumber;
+
+//! Project version string for EZXAudio.
+FOUNDATION_EXPORT const unsigned char EZXAudioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZXAudio/PublicHeader.h>
+
+
